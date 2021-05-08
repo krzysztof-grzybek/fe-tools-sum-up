43c9bbc7d1c723aadd20051f963ee9206668711e
Your Name <you@example.com>
2021-05-08 17:29:36 +0200

    add webpack

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+require('./styles.scss');
+
 const pow = prompt('Wprowadź, do której potęgi chcesz podnieść liczbę 2.');
 const result = Math.pow(2, pow);
 alert(`2 podniesione do potęgi ${pow} wynosi ${result}`);
